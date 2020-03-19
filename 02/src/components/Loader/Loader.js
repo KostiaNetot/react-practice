fb40b146cdf45f29aadff2e2ebe66d7781641a98
Kostia Netot <pugjazz@gmail.com>
2020-03-19 18:46:38 +0200

    add loader spiner..

--- 02/src/components/Loader/Loader.js
+++ 02/src/components/Loader/Loader.js
@@ -1,7 +1,16 @@
 import React from "react";
+import './Loader.css'
 
 function Loader() {
-  return <h4>Loading...</h4>;
+  // return <h4>Loading...</h4>;
+
+  return(
+    <div className="loadingio-spinner-eclipse-9l39xa59dfs">
+      <div className="ldio-8qrn4k9i40d">
+        <div></div>
+      </div>
+    </div>
+  )
 }
 
 export default Loader;
